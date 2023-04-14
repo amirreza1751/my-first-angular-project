e0d0e9b07631be60e0b789e634ee303beec510dd
amirreza1751 <dashti.amir2752@gmail.com>
2023-04-14 18:02:21 +0200

    update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -9,8 +9,16 @@ import {Recipe} from "../recipe.model";
 export class RecipeListComponent {
 
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "A test description.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
-    new Recipe("A Test Recipe", "A test description.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
-    new Recipe("A Test Recipe", "A test description.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    new Recipe("A Test Recipe1", "A test description1.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("A Test Recipe2", "A test description2.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("A Test Recipe3", "A test description3.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("A Test Recipe4", "A test description4.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("A Test Recipe5", "A test description5.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("A Test Recipe6", "A test description6.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
